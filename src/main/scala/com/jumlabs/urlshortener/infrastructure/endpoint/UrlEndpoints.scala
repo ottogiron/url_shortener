0f46e7f471498f66f21a05012fea6a85a215ba9d
Otto Giron <ottog2486@gmail.com>
2021-05-27 00:54:34 -0600

    Found how to combine two routes. Routes are Kliesli typeclases

--- src/main/scala/com/jumlabs/urlshortener/infrastructure/endpoint/UrlEndpoints.scala
+++ src/main/scala/com/jumlabs/urlshortener/infrastructure/endpoint/UrlEndpoints.scala
@@ -2,9 +2,9 @@ package com.jumlabs.urlshortener
 package infrastructure.endpoint
 
 import cats.effect.Sync
-// import cats.syntax.all._
+import cats.syntax.all._
 import io.circe.generic.auto._
-// import io.circe.syntax._
+import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
@@ -15,13 +15,18 @@ class UrlEndpoints[F[_]:Sync] extends Http4sDsl[F]{
   implicit val urlDecoder:EntityDecoder[F, Url] = jsonOf
 
   private def urlEndpoint():HttpRoutes[F] =
-    HttpRoutes.of[F]{case _ @ GET -> Root =>
+    HttpRoutes.of[F]{case _ @ GET -> Root  =>
       Sync[F].delay(Response(Status.Ok))
     }
 
-  def endpoints():HttpRoutes[F] = urlEndpoint()
+  private def urlJson(): HttpRoutes[F] =
+    HttpRoutes.of[F]{case _ @ GET -> Root / "json1" =>
+      Ok(Url("www.jumlabs.com").asJson)
+    }
+
+  def endpoints():HttpRoutes[F] =  urlEndpoint() <+> urlJson()
 }
 
 object UrlEndpoints {
-  def apply[F[_]:Sync] = new UrlEndpoints[F].endpoints()
+  def apply[F[_]:Sync]:HttpRoutes[F] = new UrlEndpoints[F].endpoints()
 }
